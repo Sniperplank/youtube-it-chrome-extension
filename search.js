395645c9f98d72c00aa3e10c61497ce1778cf8af
Eilaf Aljundi <elafjundi@gmail.com>
2022-07-23 23:50:09 -0400

    context menu bug fixed

--- search.js
+++ search.js
@@ -1,14 +1,17 @@
 let url = "https://www.youtube.com/results?search_query="
 let contextMenuItem = {
     "id": "youtubeIt",
-    "title": `Search Youtube for `,
+    "title": `Search on Youtube`,
     "contexts": ["selection"]
 }
-chrome.contextMenus.create(contextMenuItem)
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
+chrome.contextMenus.removeAll(function () {
+    chrome.contextMenus.create(contextMenuItem)
+})
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
     let searchPhrase = clickData.selectionText
     if (clickData.menuItemId == "youtubeIt" && clickData.selectionText) {
-        chrome.tabs.create({url: url + searchPhrase})
+        chrome.tabs.create({ url: url + searchPhrase })
     }
 })
